3735eb1d12c5acc1b2ab362d2417f8a74b36f01d
hyen <hyen-kim>
2020-01-08 12:56:54 +0900

    Java2 : layout -> 주석추가01

--- Java02/src/layout/component/BorderLayoutTest.java
+++ Java02/src/layout/component/BorderLayoutTest.java
@@ -10,14 +10,17 @@ import layout.window.CenterWindow;
 
 
 public class BorderLayoutTest extends JFrame {
+	// 버튼 다섯개를 배열안에 넣어둠.
 	JButton[] buts = new JButton[5];
 
 	public BorderLayoutTest() {
+		// 버튼에 간격
 		setLayout(new BorderLayout(20, 20));
+		// 버튼의 위치 : 북, 남, 동, 서, 중앙
 		String[] constraints = {"North","South", "East", "West", "Center"};
 		for(int i = 0; i < buts.length; i++) {
 			buts[i] = new JButton("버튼"+(i+1));
-			add(buts[i], constraints[i]);
+			add(buts[i], constraints[i]);		// 추가
 		}
 		int w = 300, h = 400;
 		Point p = CenterWindow.getCenterLocation(w, h);
