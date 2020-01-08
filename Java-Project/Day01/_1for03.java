8d400918b7819297f86d2bbbfc422b17aac82f3a
김혜인 <59135096+hyen-kim@users.noreply.github.com>
2020-01-09 00:08:27 +0900

    Java Project : 배열과 반복문

---
+++ Java-Project/Day01/_1for03.java
@@ -0,0 +1,16 @@
+package Day01;
+
+// for¹® È°¿ë
+
+public class _1for03 {
+	public static void main(String[] args) {
+		char A = 'A';
+		for (int i = 0; i <= 25; i++) {
+			for (int j = 0; j <= 25 - i; j++) {
+				System.out.printf("%c ", A + j);
+			}
+			System.out.println("");
+		}
+
+	}
+}
