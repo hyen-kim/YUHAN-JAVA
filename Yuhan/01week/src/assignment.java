6f6f04907d5a355a15bbb58337fa1b2617087a8d
김혜인 <k_hn_n@naver.com>
2020-03-18 15:02:12 +0900

    01week : 과제

---
+++ Yuhan/01week/src/assignment.java
@@ -0,0 +1,10 @@
+
+public class assignment {
+	
+	public static void main(String[] args) {	
+		//  학번, 학년, 이름을 콘솔 창에 출력하는 프로그램
+		System.out.println("201907033");
+		System.out.println("2nd grade");
+		System.out.println("KIMHYEIN");
+	}
+}
