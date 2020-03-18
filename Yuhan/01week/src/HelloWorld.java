66a34703e8bed07368d4f19e93d4f7e92323a3e9
김혜인 <k_hn_n@naver.com>
2020-03-18 14:56:48 +0900

    01week : HelloWorld class

---
+++ Yuhan/01week/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	
+	// main 을 빼먹었을 경우 main 쓰고 [ctrl]+[spacebar]
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 표준 출력 구문 syso 쓰고 [ctrl]+[spacebar]
+		System.out.println("Yuhan");		
+		// C언어에서의 함수를 Java에서는 메소드라고 부른다.
+		
+		System.out.println("KIM HYE IN");
+		
+	}
+
+}
