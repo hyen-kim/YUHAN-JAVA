4733a93ac8076e6b4a93503f1690894e46ee7072
김혜인 <k_hn_n@naver.com>
2020-03-25 12:12:11 +0900

    DataTypeTest : 묵시적 명시적 형 변환

--- Yuhan/02Week/src/DataTypeTest.java
+++ Yuhan/02Week/src/DataTypeTest.java
@@ -13,7 +13,7 @@ public class DataTypeTest {
 		// 98에 해당하는 문자값이 나온다는 말
 		System.out.println("c2 = " + (c2+1));	// 99가 나옴
 		// 덧셈연산을 수행하면서 데이터형이 정수형으로 바뀌었기 때문에
-		System.out.println("c2 = " + (char)(c2+1));	// c가 나옴
+		System.out.println("c2 = " + (char)(c2+1));	// c가 나옴	// <-- 강제형변환
 		// 정수형을 다시 문자형으로 형 변환한 예
 		
 		
@@ -24,13 +24,13 @@ public class DataTypeTest {
 		// byte bb = 128; 을 쓰면 에러 발생 
 		// why ? 자료형의 범위를 초과하였기 때문에 
 		// 옆에 노란색 느낌표가 뜨는 이유는 선언은 해놓고 쓰지 않았다는 의미
-		short s = bb;
+		short s = bb;		// <-- 묵시적 형변환
 		s = 128;
 		
 		int i = s;
 		i = 1000000;
 		
-		long l = i;
+		long l = i;			// <-- 묵시적 형변환 ( 작은값 -> 큰값 )
 		l = 1000000000;
 		
 		// 실수형 : float, double
@@ -41,5 +41,8 @@ public class DataTypeTest {
 		
 		double d = 6.17;
 		
+		d = 300/3;
+		System.out.println("300 / 3 = " + d); // 100.0
+		// why ? 자동형 변환이 일어났기 때문에 d의 자료형은 double이기 때문에
 	}
 }
