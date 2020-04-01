3e64e0a4a104adc01d6114febbe867835b61c33c
김혜인 <k_hn_n@naver.com>
2020-04-01 18:21:39 +0900

    PrintTest

---
+++ Yuhan/03week/src/PrintTest.java
@@ -0,0 +1,8 @@
+
+public class PrintTest {
+	public static void main(String[] args) {
+		System.out.println("Print Test");	// ¡ŸπŸ≤ﬁ O
+		System.out.print("Print Test");		// ¡ŸπŸ≤ﬁ X
+		System.out.println("....");
+	}
+}
