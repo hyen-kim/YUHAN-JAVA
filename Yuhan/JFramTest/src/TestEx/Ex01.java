4a7d789f7c7f762b23c73863064967076ef17c40
김혜인 <k_hn_n@naver.com>
2020-04-04 12:24:03 +0900

    JFram Test : 개인적인 프로젝트 준비를 위해

---
+++ Yuhan/JFramTest/src/TestEx/Ex01.java
@@ -0,0 +1,13 @@
+package TestEx;
+
+public class Ex01 {
+	/*
+	 * 기본적으로 추가해야할 것 ?
+	 * setTitle("Ex01");
+	 * setSize(300, 600); // 창 크기
+	 * setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 종료버튼 활성화
+	 * setVisible(true); // 화면에 보이기
+	 * setResizable(true); // 프레임 크기 고정
+	 * 
+	 * */
+}
