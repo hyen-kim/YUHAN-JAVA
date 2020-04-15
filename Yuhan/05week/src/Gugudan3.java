12fed37a9b6e476f93382a631c4bed377600e840
김혜인 <k_hn_n@naver.com>
2020-04-15 20:50:52 +0900

    단 입력 받고 출력받는 예제

---
+++ Yuhan/05week/src/Gugudan3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Gugudan3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.printf("단을 입력하세요 : ");
+		int dan = sc.nextInt();
+		
+		System.out.printf("구구단 %d단 입니다.\n", dan);
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("%d * %d = %d\t", dan, i, (dan*i));
+			if((i%3) == 0) {
+				System.out.println();
+			}
+		}
+	}
+}
