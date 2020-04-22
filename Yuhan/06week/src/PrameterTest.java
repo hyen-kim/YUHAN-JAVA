631fc3d110fdcd90883ce15b9bdee834d747edd8
김혜인 <k_hn_n@naver.com>
2020-04-22 21:44:26 +0900

    매개변수 2개를 전달하여 출력하는 메소드

---
+++ Yuhan/06week/src/PrameterTest.java
@@ -0,0 +1,13 @@
+// 매개변수 2개를 전달하여 출력하는 메소드
+public class PrameterTest {
+	public static void add(int n1, int n2) {
+		int result = n1 + n2;
+		System.out.printf("%d + %d = %d\n", n1, n2, result);
+	}
+	
+	public static void main(String[] args) {
+		add(11, 55);
+		add(300, 222);
+		add(2019, 33);
+	}
+}
