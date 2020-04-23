4ba146942c95f2ac6921f748ab4c897b6bfebcc7
김혜인 <k_hn_n@naver.com>
2020-04-23 11:15:13 +0900

    method update

--- Yuhan/06week/src/ParmeterTest.java
+++ Yuhan/06week/src/ParmeterTest.java
@@ -1,5 +1,5 @@
 // 매개변수 2개를 전달하여 출력하는 메소드
-public class PrameterTest {
+public class ParmeterTest {
 	public static void add(int n1, int n2) {
 		int result = n1 + n2;
 		System.out.printf("%d + %d = %d\n", n1, n2, result);
