676d600df05b67d0e761208d82706681dc432ae7
김혜인 <k_hn_n@naver.com>
2020-05-07 19:12:29 +0900

    1차 중간고사 1차 준비

---
+++ Yuhan/01st semester/src/BookStore.java
@@ -0,0 +1,109 @@
+
+public class BookStore {
+	// 속성
+	int grade; // 학년
+	private String[][] bookType = { // 학년에 따른 책에 미리 값을 선언과 동시에 초기화
+			{ "HTML", "C언어", "프로그래밍논리", "비지니스모델", "데이터구조", "데이터베이스개론", "Python", "JavaScript" },
+			{ "C++", "정보통신", "Linux", "Java", "DB응용", "전자계산기", "PHP", "JSP" },
+			{ "C#", "ASP.NET", "데이터베이스", "Android", "소프트웨어공학", "컴퓨터네트워크", "JavaFramework", "정보보안" } };
+	String book;	// 선택한 책을 저장
+	private int[][] bookMoney = { // 책에 따라 값을 선언과 동시에 초기화
+			{ 10400, 12000, 27000, 12000, 24000, 28000, 18000, 30000 },
+			{ 22100, 22400, 13200, 15800, 17000, 27000, 13100, 27000 },
+			{ 28000, 11000, 25400, 15600, 14000, 18200, 28000, 20000 } }; // 책에 따른 가격
+	int bookMoneyindex; // 선택한 책 가격
+	int paymentmethod; String pay;	// 결제방식 		
+	
+	// 기능
+	public int getGrade() { // 학년의 값을 반환
+		return grade;
+	}
+
+	public void setGrade(int grade) { // 학년의 값을 넣음
+		this.grade = grade;
+	}
+
+	public void setPaymentmethod(int paymentmethod) {
+		this.paymentmethod = paymentmethod;
+		payhan();
+	}
+
+	public String[][] getBookType() { // 책을 반환
+		return bookType;
+	}
+
+	public int[][] getBookMoney() { // 책의 값을 반환
+		return bookMoney;
+	}
+
+	public int getBookMoneyindex() {
+		return bookMoneyindex;
+	}
+
+	public void setBookMoneyindex(int bookMoneyindex) {
+		this.bookMoneyindex = bookMoneyindex;
+	}
+
+	public void displayBooks() { // 북상점 => 학년 책 가격에 대해 알려줌
+		System.out.println("=======================================");
+		System.out.println("유한대학교 ITSOFTWORE 북상점에 오신걸 환영합니다.");
+		System.out.println("=======================================");
+
+		for (int i = 0; i < bookType.length; i++) {
+			System.out.println();
+			System.out.println("< " + (i + 1) + "학년책 >");
+			for (int j = 0; j < bookMoney[i].length; j++) {
+				System.out.printf("%d. %s 책의 가격은 %d원 입니다.\n",j, bookType[i][j], bookMoney[i][j]);
+			}
+		}
+		System.out.println("\n=======================================");
+	}
+
+	public void gradeBooks() { // 선택한 학년의 책과 가격을 알려줌
+		System.out.println("=======================================");
+		System.out.println(grade + "학년책 ");
+		System.out.println("=======================================");
+
+		for (int i = 0; i < bookType[grade-1].length; i++) {
+			System.out.printf("%d. %s 책의 가격은 %d원 입니다.\n",i, bookType[grade-1][i], bookMoney[grade-1][i]);
+		}
+		System.out.println("=======================================");
+	}
+	
+	// 책과 가격을 저장
+	public void saveBooks(int bookcho) { 
+		book = bookType[grade-1][bookcho];			// 책을 저장
+		bookMoneyindex = bookMoney[grade-1][bookcho];	// 책 값을 저장
+		System.out.println(">> 구매하실 책은 " + book + "이고 책의 가격은 " + bookMoneyindex + "원 입니다.");
+	}
+	
+	// 결제 방식을 보기 좋게 한글로 변환
+	public void payhan() {
+		if(paymentmethod == 1) {
+			pay = "현금 결제";
+		}else {
+			pay = "카드 결제";
+		}
+	}
+	
+	// 현금으로 결제하였을 경우 
+	public int payMoney(int money) {
+		if(money > bookMoneyindex) {
+			System.out.printf(">> 구매가 완료되었습니다.\n잔돈은 %d원 입니다.\n", money-bookMoneyindex);
+		}else if(money < bookMoneyindex) {
+			System.out.println(">> 잔액이 부족합니다.");
+			return 0;
+		}else {
+			System.out.println(">> 구매가 완료되었습니다.");
+		}
+		return 1;
+	}
+	
+	// 구매한 내역
+	public void showInformation() {
+		System.out.println(">> 학년 : " + grade);
+		System.out.println(">> 구매한 책 : " + book);
+		System.out.println(">> 구매한 가격 : " + bookMoneyindex + "원");
+		System.out.println(">> 결제방식 : " + pay);
+	}
+}
---
+++ Yuhan/01st semester/src/RunBookStore.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class RunBookStore {
+	public static void main(String[] args) {
+		int exit = 0; // 종료를 원할 때
+		Scanner sc = new Scanner(System.in); // 입력 받을 Scanner 객체
+
+		BookStore bs = new BookStore(); // 객체 생성
+		bs.displayBooks(); // 처음에 한번만 display
+
+		while (exit != 999) {
+			// 학년을 입력받는 부분
+			System.out.print(">> 학년을 입력하세요 : ");
+			int gradenu = sc.nextInt();
+
+			// 학년을 제대로 입력했는지 확인하는 조건문
+			if (gradenu == 1 || gradenu == 2 || gradenu == 3) {
+				bs.setGrade(gradenu);
+				// 학년에 관련된 책과 가격을 출력
+				bs.gradeBooks();
+
+				// 몇번째 책을 구입할건지 입력하는 부분
+				System.out.print(">> 몇번째 책을 구입할건지 입력해주세요.(0~7) : ");
+				int bookcho = sc.nextInt();
+
+				if (bookcho >= 0 && bookcho <= 7) {
+					// 구매할 책에 대한 번호를 제대로 입력했는지
+					bs.saveBooks(bookcho);
+
+					// 결제방식을 물어보는 부분
+					System.out.print(">> 결제방식을 선택하여주세요. (0: 카드결제, 1: 현금결제) : ");
+					int pay = sc.nextInt();
+
+					if (pay == 0 || pay == 1) {
+						bs.setPaymentmethod(pay);
+						if (pay == 1) { // 결제 방식이 현금 인경우
+							System.out.print(">> 금액 입력 : ");
+							int money = sc.nextInt();
+							int moneysuccess = bs.payMoney(money);
+							if (moneysuccess == 0) { // 결제가 실패하였을 경우
+
+							}
+						} else { // 결제 방식이 카드 인경우
+							System.out.println(">> 결제가 완료되었습니다.");
+						}
+					} else {
+						System.out.printf(">> 결제 방식을 잘못 입력하셨습니다.\n다시 한 번 입력해주세요.");
+						continue;
+					}
+
+				} else {
+					System.out.printf(">> 구매하고자 하는 책이 존재하지 않습니다.\n확인 후 다시 한 번 입력해주세요.\n");
+					continue;
+				}
+
+			} else { // 제대로 입력하지 않았다면 다시 입력 받게
+				System.out.printf(">> 학년을 잘못입력하셨습니다.\n 다시 한 번 입력해주세요.\n");
+				continue;
+			}
+			// 구매 내역 출력을 원하는지 물어보는 부분
+			System.out.print(">> 구매 내역 출력을 원하십니까 ? (예 : 1, 아니요 : 아무거나) : ");
+			int show = sc.nextInt();
+			if (show == 1) {
+				bs.showInformation();
+			}
+
+			System.out.print(">> 종료를 원하십니까 ? (예 : 999, 아니요 : 아무거나 입력) : ");
+			exit = sc.nextInt();
+		}
+		System.out.println("\n\n\n\n>> 이용해주셔서 감사합니다.");
+		sc.close();
+	}
+}
