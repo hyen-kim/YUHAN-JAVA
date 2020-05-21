0d729e61a576258d7f6522427a77fdf967bfcbb6
김혜인 <k_hn_n@naver.com>
2020-05-21 19:42:57 +0900

    ThisTest1  실습

--- Yuhan/10week/src/ThisTest1.java
+++ Yuhan/10week/src/ThisTest1.java
@@ -1,6 +1,9 @@
 
 public class ThisTest1 {
 	private double n;
+	
+	// 다형성으로 인해서 동일한 이름으로 변수명을 사용할 수 있다.
+	// 필드와 매개변수(지역변수)는 동일한 이름일 경우 서로를 구별하기 위해서 필드앞에 this 키워드를 붙인다.
 	public void power(double n) {
 		this.n =  n * n;
 		System.out.println(n + "의 제곱승은 " + this.n + "입니다.");
