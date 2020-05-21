47ddd20b05af2dd89c088b37f4cda28b4693d19a
김혜인 <k_hn_n@naver.com>
2020-05-21 19:42:36 +0900

    StInfo 실습

---
+++ Yuhan/10week/src/RunStInfo.java
@@ -0,0 +1,13 @@
+
+public class RunStInfo {
+
+	public static void main(String[] args) {
+		SubstInfo sub = new SubstInfo();
+		
+		sub.name = "성준향";
+		sub.stId = "yuhan";
+		sub.age = 17;
+		sub.addr = "서울시 구로구 대림동";
+		
+	}
+} 
---
+++ Yuhan/10week/src/StInfo.java
@@ -0,0 +1,9 @@
+// Super Class (부모 클래스)
+public class StInfo {
+	
+	// 멤버필드
+	String name;
+	String stId;
+	int age;
+	
+}
